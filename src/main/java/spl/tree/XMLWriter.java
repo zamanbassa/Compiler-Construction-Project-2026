d4f93790ca01e9b3f089d416bf7d569dcbbf1d1c
babychucks <u24730841@tuks.co.za>
2026-09-19 15:19:30 +0200

    Started XMLWriter :)

--- src/main/java/spl/tree/XMLWriter.java
+++ src/main/java/spl/tree/XMLWriter.java
@@ -1,7 +1,29 @@
 package spl.tree;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class XMLWriter {
+    private static final String XML_DECLARATION = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    private static final String INDENT = "  ";
+
     public void write(TreeNode root, String outputPath) {
-        // TODO: Serialize syntax tree to XML
+        if (root == null) {
+            throw new IllegalArgumentException("Root node cannot be null");
+        }
+
+        try (FileWriter writer = new FileWriter(outputPath)) {
+            writer.write(XML_DECLARATION);
+            writer.write("<tree>\n");
+            writeNode(writer, root, 1);
+            writer.write("</tree>\n");
+        } catch (IOException e) {
+            System.err.println("Error writing XML to " + outputPath + ": " + e.getMessage());
+            e.printStackTrace();
+        }
     }
+
+    private void writeNode(FileWriter writer, TreeNode node, int depth) throws IOException {}
+    
 }
